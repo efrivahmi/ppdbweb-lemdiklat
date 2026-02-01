f3b84f96a5b164f7f0f14bff31de02099e44969e
efrivahmi <efri.crb@gmail.com>
2026-02-01 10:34:19 +0700

    feat: Set up Livewire and Alpine.js with CSP, configure Livewire asset injection, and initialize SweetAlert2.

---
+++ config/livewire.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Livewire Config
+    |--------------------------------------------------------------------------
+    |
+    | Here you can enable or disable the automatic injection of assets
+    | into your layouts. By default, Livewire injects scripts and
+    | styles into your layout automatically.
+    |
+    */
+
+    'inject_assets' => false,
+
+];
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,9 @@
 import './bootstrap';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import Csp from '@alpinejs/csp';
 import Swal from 'sweetalert2'
+
+Alpine.plugin(Csp);
+Livewire.start();
 
 window.Swal = Swal
