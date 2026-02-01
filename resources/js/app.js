dc213ee053f90d2fe04a1baab1d6ce7cc7181fd1
efrivahmi <efri.crb@gmail.com>
2026-02-01 12:24:44 +0700

    feat: Implement initial landing page with Livewire components and integrate frontend dependencies.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,9 @@
 import './bootstrap';
-import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import Csp from '@alpinejs/csp';
+import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import Alpine from '@alpinejs/csp';
 import Swal from 'sweetalert2'
 
-// Fix for production build where Csp might be wrapped in 'default'
-Alpine.plugin(Csp.default || Csp);
+window.Alpine = Alpine;
 Livewire.start();
 
 window.Swal = Swal
